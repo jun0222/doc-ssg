fca379f9ccc9edaadce32a980c0ddec27e410b93
JunHatanaka <nextseason6@gmail.com>
2025-06-27 06:12:11 +0900

    execute save.sh

--- sh/save.sh
+++ sh/save.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+git add .
+echo "=== HTMLビルド前、念の為変更をローカルgitにステージングしました。 ==="
+git commit -m "execute save.sh"
+echo "=== HTMLビルド前、念の為変更をローカルgitにコミットしました。 ==="
 node ./main.js
 echo "=== inputのmdファイルからHTMLファイルを生成しました。 ==="
 git add .
