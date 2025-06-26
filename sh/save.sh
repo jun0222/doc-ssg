f1f25e6e9c43db39eeab214c6581eabda85f2d63
JunHatanaka <nextseason6@gmail.com>
2025-06-27 06:13:54 +0900

    保存: HTMLビルド前

--- sh/save.sh
+++ sh/save.sh
@@ -3,11 +3,15 @@
 
 git add .
 echo "=== HTMLビルド前、念の為変更をローカルgitにステージングしました。 ==="
+
 git commit -m "保存: HTMLビルド前"
 echo "=== HTMLビルド前、念の為変更をローカルgitにコミットしました。 ==="
+
 node ./main.js
 echo "=== inputのmdファイルからHTMLファイルを生成しました。 ==="
+
 git add .
 echo "=== 変更をローカルgitにステージングしました。 ==="
+
 git commit -m "保存: HTMLビルド後"
-echo "=== 変更をローカルgitにコミットしました。 ==="
+echo "=== 変更をローカルgitにコミットしました。（HTMLに変化がない場合はコミットされません。） ==="
